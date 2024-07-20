a029b5450799953aeab05cbabbc953f0a3083644
walinga <matthew.walinga@gmail.com>
2024-07-20 15:59:58 -0400

    Improve error handling

--- dist/translate.js
+++ dist/translate.js
@@ -1,4 +1,8 @@
-function handleSuccess(body) {
+function handleSuccess(payload) {
+    const body = JSON.parse(payload.body);
+    console.log("body", body);
+    console.log("source", body.source);
+
     document.getElementById('quote-box').style.display = 'block';
     document.getElementById('matched-quote').textContent = body.quote;
     document.getElementById('translated-quote').textContent = body.translation;
@@ -11,7 +15,7 @@ function performTranslation(messageId, quote) {
         return;
     }
 
-    if (!/^\w+\-\w+$/.test(messageId)) {
+    if (!/^\w+\-\w+$/.test(messageId) || messageId.length > 15) {
         alert('Invalid message identifier. Please use the format 50-0505M');
         return;
     }
@@ -22,8 +26,19 @@ function performTranslation(messageId, quote) {
     }
 
     return translateQuote(messageId, quote)
-        .then(body => handleSuccess(body))
-        .catch(() => {
+        .then(payload => {
+            console.log("Response payload", payload);
+            if (payload.statusCode === 200) {
+                return handleSuccess(payload);
+            }
+
+            if (payload.error) {
+                document.getElementById('error-text').textContent = payload.error;
+            }
+            throw new Error('An error occured while performing translation:' + JSON.stringify(payload));
+        })
+        .catch(err => {
+            console.log(err)
             document.getElementById('error-text').style.display = 'block'
         })
 }
@@ -32,6 +47,7 @@ async function onTranslateClick() {
     document.getElementById('quote-box').style.display = 'none';
     document.getElementById('error-text').style.display = 'none'
     document.getElementById('loader').style.display = 'block';
+    document.getElementById('error-text').textContent = 'An error occurred. Please try again later';
 
     const messageId = document.getElementById('message-id-input').value;
     const quote = document.getElementById('quote-input').value;
