1236292b043a6595a7eea5d30fba94f300528a65
walinga <matthew.walinga@gmail.com>
2024-07-28 15:40:50 -0400

    WIP: multiple quote support

--- translate.js
+++ translate.js
@@ -9,25 +9,27 @@ function handleSuccess(payload) {
     document.getElementById('translation-source').textContent = body.source;
 }
 
+/**
+ * Splits the quote input into the individual quotes and extracts the message IDs.
+ * Returns a map of messageId -> quote
+ */
+function splitQuotes(quoteInput) {
+    // TODO: Add helpful error message for CAB-03 messages
+    const messageIdMatches = [...quoteInput.matchAll(/\d{2}\-\d{4}\w?/g), {index: quoteInput.length}];
+    const quoteMap = new Map();
+    for (let i = 0; i < messageIdMatches.length - 1; i++) {
+        const data = messageIdMatches[i];
+        const index = data.index;
+        const endIndex = messageIdMatches[i+1].index;
+        quoteMap.set(data[0], quoteInput.slice(index, endIndex));
+    }
+    return quoteMap;
+}
+
 function performTranslation(messageId, quote) {
-    if (messageId.replace(/\s/g, '') === '') {
-        alert('A message identifier must be entered');
-        return;
-    }
-
-    if (!/^\w+\-\w+$/.test(messageId) || messageId.length > 15) {
-        alert('Invalid message identifier. Please use the format 50-0505M');
-        return;
-    }
-
-    if (quote.replace(/\s/g, '') === '') {
-        alert('A quote must be entered');
-        return;
-    }
-
     return translateQuote(messageId, quote)
         .then(payload => {
-            console.log("Response payload", payload);
+            // console.log("Response payload", payload);
             if (payload.statusCode === 200) {
                 return handleSuccess(payload);
             }
@@ -37,10 +39,39 @@ function performTranslation(messageId, quote) {
             }
             throw new Error('An error occured while performing translation:' + JSON.stringify(payload));
         })
+        // TODO: Extend error functionality for multiple quotes
         .catch(err => {
             console.log(err)
             document.getElementById('error-text').style.display = 'block'
         })
+}
+
+async function translateQuotes(messageIdInput, quoteInput) {
+    const messageIdProvided = messageIdInput.replace(/\s/g, '') !== '';
+    if (messageIdProvided && (!/^\w+\-\w+$/.test(messageIdInput) || messageIdInput.length > 15)) {
+        alert('Invalid message identifier. Please use the format 50-0505M');
+        return;
+    }
+
+    if (quoteInput.replace(/\s/g, '') === '') {
+        alert('A quote must be entered');
+        return;
+    }
+
+    const quoteMap = splitQuotes(quoteInput);
+    console.log('quoteMap', quoteMap);
+
+    // TODO: Double-check existing functionality (message ID + quote entered)
+    if (quoteMap.size === 0 && !messageIdProvided) {
+        alert('Could not extract message ID');
+        return;
+    }
+
+    Promise.allSettled([...quoteMap.keys()].map(messageId =>
+        performTranslation(messageId, quoteMap.get(messageId))
+    )).then((results) => {
+        results.forEach((result) => console.log(result.status))
+    })
 }
 
 async function onTranslateClick() {
@@ -49,10 +80,9 @@ async function onTranslateClick() {
     document.getElementById('loader').style.display = 'block';
     document.getElementById('error-text').textContent = 'An error occurred. Please try again later';
 
-    const messageId = document.getElementById('message-id-input').value;
-    const quote = document.getElementById('quote-input').value;
-
-    await performTranslation(messageId, quote);
+    const messageIdInput = document.getElementById('message-id-input').value;
+    const quoteInput = document.getElementById('quote-input').value;
+    await translateQuotes(messageIdInput, quoteInput);
 
     document.getElementById('loader').style.display = 'none'
 }
