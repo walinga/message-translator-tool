235d49af6d38a40b0a54909ee13e14f5ce6bafbf
walinga <matthew.walinga@gmail.com>
2024-07-29 22:58:33 -0400

    Split quote into paragraphs

--- translate.js
+++ translate.js
@@ -1,14 +1,23 @@
-function handleSuccess(payload) {
+function handleSuccess(payload, parNums) {
     const body = JSON.parse(payload.body);
     console.log("body", body);
     console.log("source", body.source);
 
+    const endParNumText =
+        parNums && parNums[0] !== parNums[1] ? '-' + parNums[1] : '';
+    const parNumText =
+        parNums
+            ? ' (Paragraph ' + parNums[0] + endParNumText + ')'
+            : '';
     const quoteBox = document.getElementById('matched-quote');
-    quoteBox.textContent = quoteBox.textContent.concat(body.messageInfo.englishTitle, '\r\n', body.quote, '\r\n\r\n');
+    quoteBox.textContent = quoteBox.textContent.concat(body.messageInfo.englishTitle, parNumText, '\r\n', body.quote, '\r\n\r\n');
     const translationBox = document.getElementById('translated-quote');
-    translationBox.textContent = translationBox.textContent.concat(body.messageInfo.spanishTitle, '\r\n', body.translation, '\r\n\r\n');
+    translationBox.textContent = translationBox.textContent.concat(body.messageInfo.spanishTitle, parNumText, '\r\n', body.translation, '\r\n\r\n');
+
     const sourceBox = document.getElementById('translation-source');
-    sourceBox.textContent = sourceBox.textContent.concat(body.source, '\r\n');
+    if (!sourceBox.textContent.includes(body.source)) {
+        sourceBox.textContent = sourceBox.textContent.concat(body.source, '\r\n');
+    }
 }
 
 function handleError(messageId, payload) {
@@ -19,36 +28,63 @@ function handleError(messageId, payload) {
     translationBox.textContent = translationBox.textContent.concat(messageId, '\r\n', errorText, '\r\n\r\n');
 }
 
+function isEmpty(s) {
+    return s.replace(/\s/g, '') === '';
+}
+
+/**
+ * Modifies quoteMap in-place to split a chunk into multiple paragraphs.
+ */
+function splitParagraphs(messageId, chunk, quoteMap) {
+    const parNumMatches = [...chunk.matchAll(/\n(\d+) +(?=\w)/g)];
+    if (parNumMatches.length === 0) {
+        quoteMap.push([messageId, chunk]);
+        return;
+    }
+
+    // console.log(parNumMatches);
+    for (let i = 0; i < parNumMatches.length; i++) {
+        const match = parNumMatches[i];
+        let currentMatch = match;
+        let endMatch = parNumMatches[i+1];
+        while (endMatch && Number(currentMatch[1]) + 1 === Number(endMatch[1])) {
+            i++;
+            currentMatch = endMatch;
+            endMatch = parNumMatches[i+1];
+        }
+
+        const startIndex = match.index + match[0].length;
+        const endIndex = endMatch ? endMatch.index : chunk.length;
+        const paragraph = chunk.slice(startIndex, endIndex);
+        // console.log(match[1], paragraph);
+        // console.log('================');
+        if (!isEmpty(paragraph)) {
+            quoteMap.push([messageId, paragraph, [match[1], parNumMatches[i][1]]]);
+        }
+    }
+}
+
 /**
  * Splits the quote input into the individual quotes and extracts the message IDs.
  * Returns a map of messageId -> quote
  */
 function splitQuotes(quoteInput) {
     const messageIdMatches = [...quoteInput.matchAll(/(\d{2}\-\d{4}\w?)|(\w{3}-\d{2})/g), {index: quoteInput.length}];
-    const quoteMap = new Map();
+    const quoteMap = [];
     for (let i = 0; i < messageIdMatches.length - 1; i++) {
         const data = messageIdMatches[i];
         const index = data.index;
         const endIndex = messageIdMatches[i+1].index;
-        const chunk = quoteInput.slice(index, endIndex);
-        // Adjust the start of the quote to remove the paragraph number
-        const parNumMatch = chunk.match(/(?<=\s\d+)\s/);
-        const quote = parNumMatch && parNumMatch.index - index < 100
-            ? chunk.slice(parNumMatch.index)
-            : chunk;
-        quoteMap.set(data[0], quote);
+        const quote = quoteInput.slice(index, endIndex);
+        splitParagraphs(data[0], quote, quoteMap)
     }
     return quoteMap;
 }
 
-function performTranslation(messageId, quote) {
+function performTranslation(messageId, quote, parNums) {
     return translateQuote(messageId, quote)
         .then((payload) => {
-            return { payload, messageId };
-        })
-        .catch(err => {
-            console.log(err)
-            document.getElementById('error-text').style.display = 'block'
+            return { payload, messageId, parNums };
         })
 }
 
@@ -73,22 +109,27 @@ function translateQuotes(messageIdInput, quoteInput) {
             return;
         }
 
-        quoteMap.set(messageIdInput, quoteInput);
+        quoteMap.push([messageIdInput, quoteInput]);
     }
 
-    return Promise.allSettled([...quoteMap.keys()].map(messageId =>
-        performTranslation(messageId, quoteMap.get(messageId))
-    )).then((results) => {
+    return Promise.allSettled(quoteMap.map(([messageId, quote, parNums]) => {
+        // console.log('quoteMap entry', messageId, quote, parNums)
+        return performTranslation(messageId, quote, parNums)
+    })).then((results) => {
         document.getElementById('quote-box').style.display = 'block';
         results.forEach((result) => {
             const payload = result.value.payload;
             const messageId = result.value.messageId;
             if (payload.statusCode === 200) {
-                handleSuccess(payload);
+                handleSuccess(payload, result.value.parNums);
             } else {
                 handleError(messageId, payload);
             }
         })
+    })
+    .catch(err => {
+        console.log(err)
+        document.getElementById('error-text').style.display = 'block'
     })
 }
 
